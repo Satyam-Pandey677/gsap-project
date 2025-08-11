9805e0c530f2db250ca6022967fe154bddab6ed5
Satyam-Pandey677 <satyampandey0241@gmail.com>
2025-08-11 23:22:21 +0530

    feat: Add GSAP scroll-triggered animation for video playback in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,9 +44,28 @@ const Hero = () => {
         .to('.right-leaf', {y:200},0)
         .to('.left-leaf', {y:200},0)
 
+        
+
 
         const startValue =isMobile?'top 50%': 'center 60%'
         const endValue = isMobile?'120% top': 'bottom top'
+
+        let t1 = gsap.timeline({
+            scrollTrigger:{
+                trigger:"video",
+                start:startValue,
+                end:endValue,
+                scrub:true,
+                pin:true
+            }
+        })
+
+        videoRef.current.onloadedmetadata = () => {
+            t1.to(videoRef.current, {
+                currentTime:videoRef.current.duration
+            });
+        };
+
     },[])
 
   return (
@@ -84,7 +103,7 @@ const Hero = () => {
         <div className='video absolute inset-0'>
             <video 
             ref={videoRef}
-                src="/videos/input.mp4"
+                src="/videos/output.mp4"
                 muted
                 playsInline
                 preload='auto'
