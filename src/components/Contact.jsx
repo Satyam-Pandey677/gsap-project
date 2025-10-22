cdf04191d9fffd71c3a2597baf563b46ec4ba668
Satyam-Pandey677 <satyampandey0241@gmail.com>
2025-10-22 13:54:33 +0530

    changes

---
+++ src/components/Contact.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { openingHours, socials } from '../../constants'
+import { useGSAP } from '@gsap/react';
+import { SplitText } from 'gsap/all';
+import gsap from 'gsap';
+
+const Contact = () => {
+  useGSAP(() => {
+		const titleSplit = SplitText.create('#contact h2', { type: 'words' });
+		
+		const timeline = gsap.timeline({
+		 scrollTrigger: {
+			trigger: '#contact',
+			start: 'top center',
+		 },
+		 ease: "power1.inOut"
+		})
+	 
+	 timeline
+		.from(titleSplit.words, {
+		 opacity: 0, yPercent: 100, stagger: 0.02
+	 })
+		.from('#contact h3, #contact p', {
+			opacity: 0, yPercent: 100, stagger: 0.02
+	 })
+		.to('#f-right-leaf', {
+		 y: '-50', duration: 1, ease: 'power1.inOut'
+	 }).to('#f-left-leaf', {
+		 y: '-50', duration: 1, ease: 'power1.inOut'
+	 }, '<')
+	})
+  return (
+    <footer id='contact'>
+      <img src="/images/footer-right-leaf.png" alt="right-leaf" id='f-right-leaf' />
+      <img src="/images/footer-left-leaf.png" alt="left-leaf" id='f-left-leaf' />
+
+      <div className='content'>
+        <h2>Where To Find Us</h2>
+
+        <div>
+          <h3>Visit Our Bar</h3>
+          <p>435, Req Blvd. #404, Los Angeles, CA 902014</p>
+        </div>
+
+        <div>
+          <h3>Contact Us</h3>
+          <p>32646-56565</p>
+          <p>alex@gmail.com</p>
+        </div>
+
+        <div>
+          <h2>Open Every Day</h2>
+          {openingHours.map((time) => (
+            <p key={time.day}>
+              {time.day} : {time.time}
+            </p>
+          ))}
+        </div>
+
+        <div>
+          <h3>Social</h3>
+          <div className='flex-center gap-5'> 
+            {socials.map((social) => (
+              <a
+                key={social.name} 
+                href={social.url}
+                target='_blank'
+                rel='noopener noreferrer'
+                aria-label={social.name}
+              >
+                <img src={social.icon} alt="" />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Contact
